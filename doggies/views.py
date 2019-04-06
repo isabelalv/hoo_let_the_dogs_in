6a84b65adb2d74d702aea9446e03e8d7a2a78695
Isabel Alvarez <iag1997@gmail.com>
2019-04-06 13:17:37 -0400

    database

--- doggies/views.py
+++ doggies/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'doggies/index.html')
--- hoo_let_the_dogs_in/settings.py
+++ hoo_let_the_dogs_in/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'doggies.apps.DoggiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
