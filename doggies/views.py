0e719f8bebe6d21f2a32c2a50fba4d262ad2ff44
Isabel Alvarez <iag1997@gmail.com>
2019-04-06 14:21:27 -0400

    url mapping set up

--- doggies/views.py
+++ doggies/views.py
@@ -4,3 +4,9 @@ from django.template import loader
 
 def index(request):
     return render(request, 'doggies/index.html')
+
+def search(request):
+    return render(request, 'doggies/search.html')
+
+def filter(request):
+    return render(request, 'doggies/filter.html')
